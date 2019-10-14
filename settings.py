a8863f31202eb52a4d82d5c25dad0659cedaa49d
Acksell <axelen@kth.se>
2019-10-14 22:22:29 +0200

    Updated the auth flow to use pickle. Tabs -> spaces. Added requirements.txt

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 USERNAME='USERNAME'
 # SPREADSHEET_ID = "12YdppOoZUNZxhXvcY_cRgfXEfRnR_izlBsF8Sin3rw4"
 SPREADSHEET_ID = "1QPS8brN-YLFvryIgnaGXgxvFPSXZTH6EKRwXANizVCo"
-variables = ['base_tax','raw_development','military_strength','treasury','estimated_monthly_income','non_overseas_development','manpower',
+variables = ['base_tax','raw_development','treasury','estimated_monthly_income','non_overseas_development','manpower',
                'num_of_provinces_in_states','num_of_provinces_in_territories',  'development'] 
 tags = ['FRA','CAS','ENG','TUR','BUR','HAB', 'POL','ARA']
